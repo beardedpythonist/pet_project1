9fc57723d662591e07dabec56301076da7bf11fa
Alexander Gordey <alexnickgordey3@gmail.com>
2023-03-15 17:18:57 +0300

    все ок.

--- parcermain.py
+++ parcermain.py
@@ -5,15 +5,14 @@ import json
 # получаем данные
 response = requests.get('https://i-shop.by/data/products.json.gz')
 
-
 # сохраняем данные в HTML-файл  (на всякий случай)  и присваиваем переменную
-with open('iphone_data.html','w', encoding='UTF-8') as file:
+with open('iphone_data.html', 'w', encoding='UTF-8') as file:
     file.write(response.text)
 
 with open('iphone_data.html', encoding='UTF-8') as file:
     src = file.read()
 
-#преобазуем в json - файл
+# преобазуем в json - файл
 data = json.loads(src)
 
 # вытягиваем данные из вложенных словарей и сохраняем в новый  удобный список из словарей
@@ -24,15 +23,14 @@ for c in range(len(data)):
         'цена модели: ': data[c]['price'],
     })
     if len(data[c]["variants"]) >= 1:
-        for q in range(len("variants")-2):
+        for q in range(len("variants") - 2):
             final_data.append(
-            { 'модель': data[c]["variants"][q]['name'],
-              'старая цена': data[c]["variants"][q]['old_price'],
-              'новая цена': data[c]["variants"][q]['price']
-            }
-        )
+                {'модель': data[c]["variants"][q]['name'],
+                 'старая цена': data[c]["variants"][q]['old_price'],
+                 'новая цена': data[c]["variants"][q]['price']
+                 }
+            )
 
 # данные  о ценах  и моделях  хранятся в файле "new_data.json"
 with open('new_data.json', 'w', encoding='UTF-8') as f:
     json.dump(final_data, f, indent=4, ensure_ascii=False)
-
